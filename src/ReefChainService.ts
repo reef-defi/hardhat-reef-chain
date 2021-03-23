4f00223ea975bcba0b4b31c4c3eda45b8a646875
Žiga Franko <franko.ziga@gmail.com>
2021-03-23 17:16:18 +0100

    Base Reef chain service runner

---
+++ src/ReefChainService.ts
@@ -0,0 +1,41 @@
+import { ChildProcess, exec, spawn } from "child_process";
+
+export default class ReefChainService {
+  private reefChainPath = "";
+  private static service: ChildProcess | undefined;
+
+  constructor(chainPath: string) {
+    this.reefChainPath = chainPath;
+  }
+
+  public createService() {
+    ReefChainService.service = exec(`cd ${this.reefChainPath} && make eth`);
+  }
+
+  public stopService() {
+    if (ReefChainService.service) {
+      ReefChainService.service.kill();
+      ReefChainService.service = undefined;
+    }
+  }
+
+  // Pull reef chain from reef-chain release
+  private pullChain() { 
+
+    this.buildChain();
+   }
+
+  private buildChain() {
+    return new Promise((resolve, reject) => {
+      const build = spawn(`cd ${this.reefChainPath} && make build`);
+
+      build.once("close", () => {
+        resolve(1);
+      });
+      build.once("error", () => {
+        reject();
+      });
+    })
+  }
+
+}
