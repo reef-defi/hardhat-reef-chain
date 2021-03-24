80c02ecc33a523578ee28736a4f1803ad10c0560
Žiga Franko <franko.ziga@gmail.com>
2021-03-24 01:13:25 +0100

    Task names in a more friendly format

--- src/task-names.ts
+++ src/task-names.ts
@@ -1,3 +1,3 @@
-export const RUN_REEF_CHAIN = "RUN_REEF_CHAIN";
-export const RUN_REEF_SCRIPT = "RUN_REEF_SCRIPT";
-export const STOP_REEF_CHAIN = "STOP_REEF_CHAIN";
+export const RUN_REEF_SCRIPT = "reefrun";
+export const RUN_REEF_CHAIN = "run-reef-chain";
+export const STOP_REEF_CHAIN = "stop-reef-chain";
