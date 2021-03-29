c2c0158938d2ae44e779501921a4ea5652b2879d
Žiga Franko <franko.ziga@gmail.com>
2021-03-29 11:50:14 +0200

    Script runner moved to Hardhat-bodhi and run reef chain renamed

--- src/task-names.ts
+++ src/task-names.ts
@@ -1,3 +1,2 @@
-export const RUN_REEF_SCRIPT = "run";
-export const RUN_REEF_CHAIN = "run-reef-chain";
+export const RUN_REEF_CHAIN = "start-reef-chain";
 export const STOP_REEF_CHAIN = "stop-reef-chain";
